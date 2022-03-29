aee8c0bc09a5795609b5efca2b8a2ca251074918
Rodrigo <rodvalenciaa@gmail.com>
2022-03-28 18:52:54 -0700

    starter code

---
+++ __tests__/Portion.test.js
@@ -0,0 +1,8 @@
+const Portion = require('../lib/Potion.js');
+
+test('creates a health potion object', () => {
+    const potion = new Potion('health');
+
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
